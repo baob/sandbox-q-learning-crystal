5d79b2cfef1f3cb2c68ecbfb42c52f1be4292426
baob <coder@onesandthrees.com>
2017-09-01 21:33:04 +0100

    add series model for running training then evaluation

---
+++ lib/series.rb
@@ -0,0 +1,48 @@
+# Runs a series of games silently, then 1000 more to
+# evaluate effectiveness.
+# explore_percent is set to 0 for the evaluation
+# needs to be manually reset for another series
+# TODO: improve on this ^
+#
+require 'game'
+
+class Series
+
+  attr_reader :player_1_policy, :player_2_policy, :repeats, :trace, :stats
+
+  def initialize(player_1_policy, player_2_policy, repeats, trace: false)
+    @player_1_policy = player_1_policy
+    @player_2_policy = player_2_policy
+    @repeats = repeats
+    @trace = trace
+    @stats = Hash.new(0)
+  end
+
+  def play
+    repeats.times { Game.new(player_1_policy, player_2_policy, trace: trace).play }
+
+    [player_1_policy, player_2_policy].each do |policy|
+      policy.explore_percent = -1 if policy.respond_to?(:explore_percent)
+    end
+
+    evaluation_count.times do
+
+      player = Game.new(player_1_policy, player_2_policy, trace: trace).play
+      if player.nil?
+        stats[:draws] += 1
+      else
+        if player.policy == player_1_policy
+          stats[:player_1] += 1
+        else
+          stats[:player_2] += 1
+        end
+      end
+    end
+
+    puts "Stats: #{stats}"
+  end
+
+  def evaluation_count
+    1000
+  end
+end
