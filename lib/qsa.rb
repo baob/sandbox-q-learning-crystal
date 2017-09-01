4c84b39f52b7d6c5639976726a48184ff0777a3e
baob <coder@onesandthrees.com>
2017-09-01 19:26:55 +0100

    add some stats to qsa

--- lib/qsa.rb
+++ lib/qsa.rb
@@ -4,12 +4,14 @@
 class Qsa
   def initialize
     @qsa = {}
+    @stats = Hash.new(0)
   end
 
   def set(state, move, player, new_q)
     @qsa[state] ||= {}
     @qsa[state][move] ||= {}
     @qsa[state][move][player] = new_q
+    increment_sets
   end
 
   def get(state, move, player)
@@ -26,7 +28,25 @@ class Qsa
 
   def inspect
     "<Qsa:0x#{object_id.to_s(16)}"\
+      " @stats=\"#{@stats}\""\
+      " states.count=\"#{@qsa.values.count}\""\
+      " values.count=\"#{values_count}\""\
     '>'
+  end
+
+  def values_count
+    @qsa.reduce(0) do |total, (k_state, v_state)|
+      v_state_size = v_state.reduce(0) do |total, (k_action, v_action)|
+        total += v_action.size # accumulating no players in each action
+        total
+      end
+      total += v_state_size # accumulating no players in each state
+      total
+    end
+  end
+
+  def increment_sets
+    @stats[:sets] += 1
   end
 
 end
