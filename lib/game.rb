0dd0f5313842f967cc5ecbd8a99a829c7085e938
baob <coder@onesandthrees.com>
2017-09-01 20:32:46 +0100

    tidying up Game

--- lib/game.rb
+++ lib/game.rb
@@ -14,11 +14,11 @@ class Game
       policy: player_1_policy,
       token:  'X'
     )
-
     @players << Player.new(
       policy: player_2_policy,
       token:  'O'
     )
+
     @players[0].other_player = @players[1]
     @players[1].other_player = @players[0]
 
@@ -27,21 +27,21 @@ class Game
 
   def play
     @board ||= Board.new
-    move = 0
     player = @players.sample
     player.number = 1
     player.other_player.number = 2
     tokens = player.token + player.other_player.token
+
     loop do
       puts "\nplayer making a move #{player}" if @trace
       @board = player.policy.play(@board, player.number)
       puts @board.to_s(tokens) if @trace
       break if @board.game_over?
-      move += 1
       player = player.other_player
     end
+
     if w = @board.winner
-      player = @players.select { |p| p.number == w }.first
+      player = @players.detect { |p| p.number == w }
       puts "\n\nplayer #{w} won, #{player}" if @board.is_win_for?(player.number) if @trace
       return player
     else
