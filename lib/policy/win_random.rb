082172406d76329cc514e66165a3d8f8dd2e183e
baob <coder@onesandthrees.com>
2017-09-08 00:25:47 +0100

    add a player that randomises (goodish) plays

---
+++ lib/policy/win_random.rb
@@ -0,0 +1,31 @@
+require 'board'
+require 'policy/win_now_block_trap'
+
+module Policy
+  class WinRandom < Base
+    class << self
+
+      def play(board, player)
+        random_win_policy.play(board, player)
+      end
+
+      private
+
+      def random_win_policy
+        [
+          WinNowBlockTrap,
+          WinNowBlockTrap,
+          WinNowBlockTrap,
+          WinNowBlockTrap,
+          WinNowBlockNaiveLine,
+          WinNowBlockNaiveLine,
+          WinNowBlockNaiveLine,
+          WinNowBlock,
+          WinNowBlock,
+          WinNow
+        ].sample
+      end
+    end
+
+  end
+end
