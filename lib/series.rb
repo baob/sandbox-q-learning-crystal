5d44672a8cd50d09853d4cfd322ab76e88d33ee8
baob <coder@onesandthrees.com>
2017-09-13 16:19:59 +0100

    dont confuse player 1/2 in series with who plays first

--- lib/series.rb
+++ lib/series.rb
@@ -8,33 +8,33 @@ require 'game'
 
 class Series
 
-  attr_reader :player_1_policy, :player_2_policy, :repeats, :trace, :stats
+  attr_reader :player_a_policy, :player_b_policy, :repeats, :trace, :stats
 
-  def initialize(player_1_policy, player_2_policy, repeats, trace: false)
-    @player_1_policy = player_1_policy
-    @player_2_policy = player_2_policy
+  def initialize(player_a_policy, player_b_policy, repeats, trace: false)
+    @player_a_policy = player_a_policy
+    @player_b_policy = player_b_policy
     @repeats = repeats
     @trace = trace
     @stats = Hash.new(0)
   end
 
   def play
-    repeats.times { Game.new(player_1_policy, player_2_policy, trace: trace).play }
+    repeats.times { Game.new(player_a_policy, player_b_policy, trace: trace).play }
 
-    [player_1_policy, player_2_policy].each do |policy|
+    [player_a_policy, player_b_policy].each do |policy|
       policy.explore_percent = -1 if policy.respond_to?(:explore_percent)
     end
 
     evaluation_count.times do
 
-      player = Game.new(player_1_policy, player_2_policy, trace: trace).play
+      player = Game.new(player_a_policy, player_b_policy, trace: trace).play
       if player.nil?
         stats[:draws] += 1
       else
-        if player.policy == player_1_policy
-          stats[:player_1] += 1
+        if player.policy == player_a_policy
+          stats[:player_a] += 1
         else
-          stats[:player_2] += 1
+          stats[:player_b] += 1
         end
       end
     end
