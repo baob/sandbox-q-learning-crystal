5b2a64dd27ae4ae6ec4abd1ef06ca1ac18609e1a
baob <coder@onesandthrees.com>
2018-09-26 18:05:00 +0100

    all tests passing

--- spec/unit/policy/win_now_spec.cr
+++ spec/unit/policy/win_now_spec.cr
@@ -1,11 +1,14 @@
 require "../../../src/policy/win_now"
 
+def policy
+  Policy::WinNow
+end
 module Policy
   describe WinNow do
 
-    subject { described_class }
+    # subject { described_class }
 
-    it_behaves_like "any policy"
+    it_behaves_like_any_policy(policy)
 
   end
 end
