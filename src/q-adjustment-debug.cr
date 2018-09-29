e3f599afebaf77e4986e9480c3c2dec4c267b809
baob <coder@onesandthrees.com>
2018-09-29 23:00:15 +0100

    tweaks to QAdjustmentDebug

--- src/q-adjustment-debug.cr
+++ src/q-adjustment-debug.cr
@@ -1,23 +1,20 @@
 require "./series"
 
-
-class QAdjustmentDebug
-end
-
 # TODO: Write documentation for `Q::Learning::Crystal`
 require "./series"
-module Q::Learning::Crystal
+
+module QAdjustmentDebug
   VERSION = "0.1.0"
 
   ql = Policy::QLearning.policy(trace: true) # set up our untrained q-learning 'policy'
   ql.discount = 0.9
   ql.explore_percent = 90
 
-  ql.learning_rate = 0.512
+  ql.learning_rate = 0.256
   Game.new(ql, Policy::WinRandom.policy, trace: true).play  # to see one game (q-learning versus good-but-a-bit-random)
 
   ql.trace = false
-  Series.new(ql, Policy::WinRandom.policy, 10).play  # to run a training series
+  Series.new(ql, Policy::WinRandom.policy, 100000).play  # to run a training series
 
   ql.trace = true
   Game.new(ql, Policy::WinRandom.policy, trace: true).play  # to see one game (q-learning versus good-but-a-bit-random)
