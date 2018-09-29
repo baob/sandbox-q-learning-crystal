739adb77bd499df60e63c9ccd444396d7b084838
baob <coder@onesandthrees.com>
2018-09-29 15:51:16 +0100

    apply learning from historical commit

--- src/q-learning-crystal.cr
+++ src/q-learning-crystal.cr
@@ -9,18 +9,42 @@ module Q::Learning::Crystal
   # > Game.new(ql, Policy::WinRandom, trace: true).play  # to see one game (q-learning versus good-but-a-bit-random)
   # > Series.new(ql, Policy::WinRandom, 500).play  # to run a training series
 
+  # Date:   Sat Sep 9 00:16:21 2017 0100
+
+  # For future ref, it looks like optimal 'training'
+  # is acheived with 50K - 300K games against varied
+  # opponents (including the completely random) with the
+  # explore % set high (say 90%) and the learning rate
+  # varing from 0.5 to 0.01 (to force convergence). Other
+  # strategies (e.g. learning rate 0.9 against consistent
+  # non-random opponent) can lead to convergence on
+  # suboptimal solutions.
+
 
   ql = Policy::QLearning.policy # set up our untrained q-learning 'policy'
-  ql.learning_rate = 0.1
   ql.discount = 0.9
-  ql.explore_percent = 67
-  Game.new(ql, Policy::WinRandom.policy, trace: true).play  # to see one game (q-learning versus good-but-a-bit-random)
+  ql.explore_percent = 90
+
+  ql.learning_rate = 0.512
   Series.new(ql, Policy::WinRandom.policy, 5000).play  # to run a training series
   ql.assess
 
-  Game.new(ql, Policy::WinRandom.policy, trace: true).play  # to see one game (q-learning versus good-but-a-bit-random)
-  Series.new(ql, Policy::Random.policy, 50000).play  # to run a training series
+  ql.learning_rate = 0.256
+  Series.new(ql, Policy::WinRandom.policy, 10000).play  # to run a training series
   ql.assess
+
+  ql.learning_rate = 0.128
+  Series.new(ql, Policy::WinRandom.policy, 20000).play  # to run a training series
+  ql.assess
+
+  ql.learning_rate = 0.064
+  Series.new(ql, Policy::WinRandom.policy, 40000).play  # to run a training series
+  ql.assess
+
+  ql.learning_rate = 0.032
+  Series.new(ql, Policy::WinRandom.policy, 80000).play  # to run a training series
+  ql.assess
+
   Game.new(ql, Policy::WinRandom.policy, trace: true).play  # to see one game (q-learning versus good-but-a-bit-random)
 
   
