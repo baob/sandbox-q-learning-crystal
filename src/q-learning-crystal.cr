952c3a1d4cd387b161b6ba44afabd08561aa0944
baob <coder@onesandthrees.com>
2018-09-29 15:24:27 +0100

    adding assesments after every series

--- src/q-learning-crystal.cr
+++ src/q-learning-crystal.cr
@@ -12,14 +12,17 @@ module Q::Learning::Crystal
 
   ql = Policy::QLearning.policy # set up our untrained q-learning 'policy'
   ql.learning_rate = 0.1
-  ql.discount = 0.3
+  ql.discount = 0.9
   ql.explore_percent = 67
   Game.new(ql, Policy::WinRandom.policy, trace: true).play  # to see one game (q-learning versus good-but-a-bit-random)
-  Series.new(ql, Policy::WinRandom.policy, 100000).play  # to run a training series
+  Series.new(ql, Policy::WinRandom.policy, 5000).play  # to run a training series
+  ql.assess
+
   Game.new(ql, Policy::WinRandom.policy, trace: true).play  # to see one game (q-learning versus good-but-a-bit-random)
   Series.new(ql, Policy::Random.policy, 50000).play  # to run a training series
+  ql.assess
   Game.new(ql, Policy::WinRandom.policy, trace: true).play  # to see one game (q-learning versus good-but-a-bit-random)
+
   
   # Indicators of training success:
-  ql.assess
 end
