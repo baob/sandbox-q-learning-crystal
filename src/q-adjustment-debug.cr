19357a3f98365b542b6dbc1b84059916906ad0e6
baob <coder@onesandthrees.com>
2018-09-30 01:56:19 +0100

    tweak src/q-adjustment-debug.cr

--- src/q-adjustment-debug.cr
+++ src/q-adjustment-debug.cr
@@ -10,16 +10,26 @@ module QAdjustmentDebug
   ql.discount = 0.9
   ql.explore_percent = 90
 
-  ql.learning_rate = 0.256
+  ql.learning_rate = 0.1
   Game.new(ql, Policy::WinRandom.policy, trace: true).play  # to see one game (q-learning versus good-but-a-bit-random)
 
+  puts "ql.qsa.qsa.inspect: ------- \n #{ql.qsa.qsa.inspect}"
+
+
+
   ql.trace = false
+  Series.new(ql, Policy::WinRandom.policy, 100).play  # to run a training series
+
+  puts "ql.qsa.qsa.inspect: ------- \n #{ql.qsa.qsa.inspect}"
+
   Series.new(ql, Policy::WinRandom.policy, 100000).play  # to run a training series
 
   ql.trace = true
   Game.new(ql, Policy::WinRandom.policy, trace: true).play  # to see one game (q-learning versus good-but-a-bit-random)
 
   ql.assess
+
+  # puts "ql.qsa.qsa_non_trivial.inspect: ------- \n #{ql.qsa.qsa.inspect}"
 
 
   
